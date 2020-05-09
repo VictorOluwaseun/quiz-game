fefcf757070b352f5bc296ca8334ef602d2a2bf3
Victor <adevic4christ@gmail.com>
2020-05-09 18:10:04 +0100

    Created Javascript structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const QuestionCtrl = (() => {
+  //Private
+  return {
+
+  }
+})();
+
+const LogicCtrl = (() => {
+  //Private
+  return {
+
+  }
+})();
+
+const UICtrl = (() => {
+  //Private
+  return {
+
+  }
+})();
+
+const App = (() => {
+  //Private
+  return {
+
+  }
+})();
